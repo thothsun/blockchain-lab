4e7a1429611fc625275be938e06fd3c59bf5c625
sunshuai <438819781@qq.com>
2019-11-17 09:42:54 +0800

    成员页面 居中

--- pages/members.vue
+++ pages/members.vue
@@ -3,13 +3,12 @@
     <myheader index="/members"></myheader>
     <el-main class="main-content">
       <el-row :gutter="0" type="flex" justify="center">
-        <el-col :xs="20" :sm="18" :md="16" :lg="14" :xl="12">
-          <h1>团队成员</h1>
+        <el-col :xs="20" :sm="18" :md="16" :lg="14" :xl="12" style="display: flex;justify-content: center;flex-wrap: wrap">
 
 
           <div
             style="margin-top:50px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
-            <div style="display: inline-block;height: 150px;width: 150px;">
+            <div style="height: 150px;width: 150px;">
               <div style="height: 100%;width: 100%">
                 <el-avatar
                   style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
@@ -38,7 +37,7 @@
 
           <div
             style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
-            <div style="display: inline-block;height: 150px;width: 150px;">
+            <div style="height: 150px;width: 150px;">
               <div style="height: 100%;width: 100%">
                 <el-avatar
                   style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
@@ -66,7 +65,7 @@
 
           <div
             style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
-            <div style="display: inline-block;height: 150px;width: 150px;">
+            <div style="height: 150px;width: 150px;">
               <div style="height: 100%;width: 100%">
                 <el-avatar
                   style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
@@ -92,7 +91,257 @@
             </div>
           </div>
 
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
 
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style="margin-top:10px; height: 200px;width: 800px;display: flex;justify-content: space-around;align-items: center;border: 1px #DCDFE6 solid; border-radius: 4px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px">
+            <div style="height: 150px;width: 150px;">
+              <div style="height: 100%;width: 100%">
+                <el-avatar
+                  style="width: 100%;height: 100%;font-size: 8rem;display: flex;justify-content: center;align-items: center">
+                  A
+                </el-avatar>
+              </div>
+            </div>
+
+            <el-divider direction="vertical"></el-divider>
+
+            <div style="display: inline-block;height: 180px;width: 580px">
+
+              <div style="height: 20%;display: flex;align-items: center;margin: 10px 0">
+                <b style="font-size: 2rem">姓名</b> &nbsp;&nbsp;&nbsp;&nbsp; <b>硕士研究生</b>
+              </div>
+
+
+              <div style="height: 60%;margin: 10px 0;overflow: auto">
+                <p style="text-indent: 2em;font-size: 1.3rem; text-align: justify;text-justify: inter-ideograph">
+                  简介
+                </p>
+              </div>
+            </div>
+          </div>
         </el-col>
       </el-row>
     </el-main>
